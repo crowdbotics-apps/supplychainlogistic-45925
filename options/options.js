ed5c11b05349966bce30e9a8c4149b951fc3c9ae
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:51:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "supplychainlogistic-45925",
+  url: "https://supplychainlogistic-45925.botics.co",
+  api: "https://supplychainlogistic-45925.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
